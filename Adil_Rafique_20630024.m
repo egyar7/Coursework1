d0ff45fcd0db62d03180220972d09edfe09a190f
egyar7 <egyar7@nottingham.ac.uk>
2025-02-06 12:15:35 +0000

    Completed Question 1

---
+++ Adil_Rafique_20630024.m
@@ -0,0 +1,22 @@
+%Adil Rafique egyar7@nottingham.ac.uk
+
+%Question 1
+
+clc, clearvars
+
+x = 1.5;
+
+a = sin(2*x);
+b = sin(x)^2;
+c = x^5 + 3*x^4 + x^3 + (x^2)/2 + x;
+d = exp(sin(x)*cos(x));
+e = (x + 1)*(x-1)*((x^2) + 1)*(x^3);
+f = a + 2*b;
+g = (f/(c + e))^2;
+
+%%
+
+%Question 2
+
+
+
